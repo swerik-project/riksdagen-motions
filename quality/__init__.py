76134d577fb5e03aa57f84027ef7ad1c8503d399
Bob Borges <robert.d.borges@gmail.com>
2026-05-11 10:42:12 +0200

    chore: cleanup quality/__init__ file

--- quality/__init__.py
+++ quality/__init__.py
@@ -8,4 +8,16 @@
 .. include: docs/qe_N-motions.md
     :start-after: Summary
     :end-before: What is the problem
+
+
+### `qe_goldstandard.py`
+.. include: docs/qe_segmentation-title-signature.md
+    :start-after: Summary
+    :end-before: What is the problem
+
+
+### `qe_ocr.py`
+.. include: docs/qe_ocr.md
+    :start-after: Summary
+    :end-before: What is the problem
 """
